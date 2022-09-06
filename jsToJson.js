61ec1df6a10fd76c0ab46cb43eb432fccd08793a
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-06 14:29:40 +0300

    JSON: JSON <=> JS Opject | .parse()  | .stringify()

---
+++ jsToJson.js
@@ -0,0 +1,8 @@
+let myJsObject2 = { userName: "Osama", Age: 36 };
+let myJsonObject2 = JSON.stringify(myJsObject);
+
+console.log(myJsObject2);
+console.log(typeof myJsObject2);
+
+console.log(myJsonObject2);
+console.log(typeof myJsonObject2);
---
+++ jsonToJS.js
@@ -0,0 +1,8 @@
+let myJsonObject = '{"userName" : "Osama", "Age" : 36}';
+let myJsObject = JSON.parse(myJsonObject);
+
+console.log(myJsonObject);
+console.log(typeof myJsonObject);
+
+console.log(myJsObject);
+console.log(typeof myJsObject);
