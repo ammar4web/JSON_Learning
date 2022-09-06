8c629c9442b3572c584d6a3b34749edf8cbd2738
Mohammd Ammar Ammar <m.ammar4web@gmail.com>
2022-09-06 14:27:25 +0300

    JSON: Syntax JSON | Syntax Object JS

---
+++ syntax.js
@@ -0,0 +1,21 @@
+let json = {
+  userName: "Ammar4web",
+  country: "Syria",
+  age: 24,
+  working: true,
+  identity: null,
+  skills: {
+    frontEnd: ["HTML", "CSS", "JS"],
+    backEnd: ["PHP", "Laravel"],
+  },
+};
+
+// JavaScript
+// value: "key": + 'key' + ...
+// key: "key": + 'key': + key:
+// we can use comments
+
+// JSON
+//  value: "key": + ...
+// key: "key":
+// we can not use comments
